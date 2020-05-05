d3554128f8adc30a7879322bfbb9f29d63fabf26
ANDRES JAHR <asjahr@uc.cl>
2020-05-04 22:32:42 -0400

    Fix ingredient path

--- hamburguesa/models.py
+++ hamburguesa/models.py
@@ -6,8 +6,6 @@ class Ingrediente(models.Model):
     nombre = models.CharField(max_length=150)
     descripcion = models.CharField(max_length=500)
 
-    def __str__(self):
-        return self.nombre
     
 class Hamburguesa(models.Model):
     nombre = models.CharField(max_length=150)
